2f342e3fdc60464d57781d6b0d4190b9f32dce27
Dayaldonadkar <Dayalpython@gmail.com>
2023-03-03 03:22:45 +0530

    api fetched

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import Movies from "./Components1/Movies";
+
 import Movies2 from "./Components1/Movies2";
 import Navbar from "./Components1/Navbar";
 
 export default class App extends Component {
-  c = "john";
   render() {
     return (
       <div className="">

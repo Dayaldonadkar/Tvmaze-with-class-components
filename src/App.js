d901dd511ec31662d99bf3293c6b404cd32c1894
Dayaldonadkar <Dayalpython@gmail.com>
2023-03-03 13:52:39 +0530

    filter added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Movies2 from "./Components1/Movies2";
 import Navbar from "./Components1/Navbar";
@@ -6,11 +7,34 @@ import Navbar from "./Components1/Navbar";
 export default class App extends Component {
   render() {
     return (
-      <div className="">
+      <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Movies2 genre="all" key="all" />} />
+          <Route
+            exact
+            path="/hollywood"
+            element={<Movies2 genre="hollywood" key="hollywood" />}
+          />
 
-        <Movies2 />
-      </div>
+          <Route
+            exact
+            path="/anime"
+            element={<Movies2 genre="anime" key="anime" />}
+          />
+          <Route
+            exact
+            path="/Bollywood"
+            element={<Movies2 genre="bollywood" key="bollywood" />}
+          />
+
+          <Route
+            exact
+            path="/funny"
+            element={<Movies2 genre="funnt" key="funny" />}
+          />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
--- src/Components1/Navbar.js
+++ src/Components1/Navbar.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
+import { BrowserRouter, Router, Routes, Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div className=" flex justify-center bg-blue-300 py-10">
-        <div className="flex justify-between w-8/12">
-          <h1>Tv Maze</h1>
-          <div className="flex space-x-7">
-            <h1>Home</h1>
-            <h1>About</h1>
+      <>
+        <div className=" flex justify-center bg-blue-300 py-10">
+          <div className="flex justify-between w-8/12">
+            <Link to="">
+              {" "}
+              <h1>Tv Maze</h1>
+            </Link>
+            <div className="flex space-x-7">
+              <h1>Home</h1>
+              <h1>About</h1>
+            </div>
           </div>
         </div>
-      </div>
+
+        <div className="flex justify-center">
+          <ul className="flex w-8/12 justify-between">
+            <Link to="/">
+              <li>All</li>
+            </Link>
+            <Link to="hollywood">Hollywood</Link>
+            <Link to="/anime">
+              <li>Anime</li>
+            </Link>
+            <li>
+              <Link to="/bollywood">Bollywood</Link>
+            </li>
+            <li>
+              <Link to="/funny">Funny</Link>
+            </li>
+            <li>
+              <Link to="/anime">Anime</Link>
+            </li>
+          </ul>
+        </div>
+      </>
     );
   }
 }
